11c3d1d9a1938d2a1aebb16f58a25efa6d258956
michelle2662 <101956495+michelle2662@users.noreply.github.com>
2024-07-07 18:09:15 -0700

    Added navigation to Welcome Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
@@ -63,6 +64,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.generativeai)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/ckroetsch/imfeelinghungry/AppNavigation.kt
@@ -0,0 +1,44 @@
+package ckroetsch.imfeelinghungry
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination ="welcome") {
+        composable("welcome") { WelcomeScreen(navController = navController) }
+        composable("restaurant") { RestaurantScreen(navController = navController) }
+        composable("food") { FoodScreen(navController = navController) }
+        composable("dietaryPreference") { DietaryPreferenceScreen(navController = navController) }
+    }
+}
+
+@Composable
+fun DietaryPreferenceScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+Text(text = "Dietary Preference")
+}
+
+@Composable
+fun FoodScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+Text(text = "Food")
+}
+
+@Composable
+fun RestaurantScreen(
+    modifier: Modifier =  Modifier,
+    navController: NavController
+) {
+    Text(text = "Restaurant")
+}

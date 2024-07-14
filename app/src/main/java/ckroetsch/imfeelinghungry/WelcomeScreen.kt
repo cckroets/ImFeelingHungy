80c59be53f24b3e6783f341b712c412d2645a8d6
michelle2662 <101956495+michelle2662@users.noreply.github.com>
2024-07-13 20:25:59 -0700

    Changed box back to column

--- app/src/main/java/ckroetsch/imfeelinghungry/WelcomeScreen.kt
+++ app/src/main/java/ckroetsch/imfeelinghungry/WelcomeScreen.kt
@@ -2,7 +2,17 @@ package ckroetsch.imfeelinghungry
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -10,9 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,74 +39,69 @@ fun WelcomeScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
 ) {
-    Box(
-        modifier = modifier
+
+
+    Column(
+        modifier
             .background(Color(0xffffd32b))
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = Modifier.align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 32.dp)
-            ) {
-                Text(
+        //Spacer for title
+        Spacer(modifier = Modifier.height(35.dp))
+        //Title
+        Text(
+            text = "I'M FEELIN' HUNGRY",
+            style = TextStyle(fontSize = 55.sp),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        )
+        //Spacer for title and image
+        Spacer(modifier = Modifier.height(10.dp))
 
-                    text = "I'M FEELIN' HUNGRY",
-                    style = TextStyle(fontSize = 55.sp),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight()
-                )
-            }
 
-            //Spacer for between title and app icon
-            Spacer(modifier = Modifier.height(25.dp))
+        //Placeholder
+        Image(
+            painter = painterResource(R.drawable.app_icon),
+            contentDescription = "App Icon",
+            modifier = Modifier
+                .size(250.dp)
+                .clip(CircleShape)
+        )
 
-            // Placeholder
-            Image(
-                painter = painterResource(R.drawable.app_icon),
-                contentDescription = "App Icon",
-                modifier = Modifier.size(250.dp)
-            )
 
-            //Spacer for between app icon and text
-            Spacer(modifier = Modifier.height(15.dp))
+        Text(
+            text = "Discover healthier alternatives to enjoy fast food and eat smart",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(55.dp))
 
+        Button(
+            shape = RoundedCornerShape(26.dp),
+            modifier = Modifier.size(width = 200.dp, height = 65.dp),
+            onClick = {
+                navController.navigate("restaurant")
+            }) {
             Text(
-                text = "Discover healthier alternatives to enjoy fast food and eat smart",
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+                text = "Get Started",
+                style = MaterialTheme.typography.titleLarge,
             )
-
-            //Spacer for between smaller text and button
-            Spacer(modifier = Modifier.height(45.dp))
-
-            Button(
-                shape = RoundedCornerShape(26.dp),
-                modifier = Modifier.size(width = 200.dp, height = 65.dp),
-                onClick = {
-                    navController.navigate("restaurant")
-                }
-            ) {
-                Text(
-                    text = "Get Started",
-                    style = MaterialTheme.typography.titleLarge,
-                )
-            }
         }
+
+
     }
 }
+
 
 @Preview
 @Composable
@@ -103,4 +110,5 @@ fun WelcomeScreenPreview() {
         val navController = rememberNavController()
         WelcomeScreen(modifier = Modifier, navController = navController)
     }
+
 }

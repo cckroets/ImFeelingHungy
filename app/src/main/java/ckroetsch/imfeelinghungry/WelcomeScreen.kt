88106ac86cf7cadbf4ec8b175fd35a0bd07514b7
michelle2662 <101956495+michelle2662@users.noreply.github.com>
2024-07-13 20:17:27 -0700

    Improved WelcomeScreen UI

--- app/src/main/java/ckroetsch/imfeelinghungry/WelcomeScreen.kt
+++ app/src/main/java/ckroetsch/imfeelinghungry/WelcomeScreen.kt
@@ -1,57 +1,106 @@
 package ckroetsch.imfeelinghungry
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import ckroetsch.imfeelinghungry.ui.theme.ImFeelingHungryTheme
 
 @Composable
 fun WelcomeScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
 ) {
-
-
-    Column(
-        modifier
+    Box(
+        modifier = modifier
+            .background(Color(0xffffd32b))
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.SpaceBetween
+            .padding(16.dp)
     ) {
-        Button(onClick = {
-            navController.navigate("restaurant")
-        }) {
-            Text(text = "Restaurant")
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp)
+            ) {
+                Text(
+
+                    text = "I'M FEELIN' HUNGRY",
+                    style = TextStyle(fontSize = 55.sp),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                )
+            }
+
+            //Spacer for between title and app icon
+            Spacer(modifier = Modifier.height(25.dp))
+
+            // Placeholder
+            Image(
+                painter = painterResource(R.drawable.app_icon),
+                contentDescription = "App Icon",
+                modifier = Modifier.size(250.dp)
+            )
+
+            //Spacer for between app icon and text
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Text(
+                text = "Discover healthier alternatives to enjoy fast food and eat smart",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+
+            //Spacer for between smaller text and button
+            Spacer(modifier = Modifier.height(45.dp))
+
+            Button(
+                shape = RoundedCornerShape(26.dp),
+                modifier = Modifier.size(width = 200.dp, height = 65.dp),
+                onClick = {
+                    navController.navigate("restaurant")
+                }
+            ) {
+                Text(
+                    text = "Get Started",
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
         }
-
-        Button(onClick = {
-            navController.navigate("food")
-        }) {
-            Text(text = "food")
-        }
-
-        Button(onClick = {
-            navController.navigate("dietaryPreference")
-        }) {
-            Text(text = "Dietary Preference")
-        }
-
-
     }
 }
-
 
 @Preview
 @Composable
 fun WelcomeScreenPreview() {
-    val navController = rememberNavController()
-    WelcomeScreen(modifier = Modifier, navController = navController)
+    ImFeelingHungryTheme {
+        val navController = rememberNavController()
+        WelcomeScreen(modifier = Modifier, navController = navController)
+    }
 }

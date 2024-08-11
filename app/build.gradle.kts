321c8a15614f9a03ceb5fe3b1e5fe36ef12ba38a
michelle2662 <101956495+michelle2662@users.noreply.github.com>
2024-08-10 18:05:48 -0700

    removed extra dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,6 @@ android {
 
 dependencies {
     api(platform("dev.chrisbanes.compose:compose-bom:2024.07.00-alpha02"))
-    implementation ("androidx.compose.material:material-icons-core:<version>")
-    implementation (libs.androidx.material.icons.extended)
-
     implementation("io.github.klassenkonstantin:physics-layout:0.4.1")
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
--- app/src/main/java/ckroetsch/imfeelinghungry/discover/DiscoverScreen.kt
+++ app/src/main/java/ckroetsch/imfeelinghungry/discover/DiscoverScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
